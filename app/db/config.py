e8926ded710eef0f36c1afd1c75abd660e49079a
Ido <idoazzz@gmail.com>
2019-10-26 13:46:11 -0400

    Add alembic

--- app/db/config.py
+++ app/db/config.py
@@ -7,12 +7,11 @@ from sqlalchemy.orm import sessionmaker
 from models import Base
 
 
+PORT = os.environ.get('EXCHANGER_PORT', '5432')
+DB = os.environ.get('EXCHANGER_DB', 'exchanger')
+HOST = os.environ.get('EXCHANGER_HOST', 'localhost')
 USERNAME = os.environ.get('EXCHANGER_USERNAME', 'exchanger')
 PASSWORD = os.environ.get('EXCHANGER_PASSWORD', 'exchanger')
-HOST = os.environ.get('EXCHANGER_HOST', 'localhost')
-DB = os.environ.get('EXCHANGER_DB', 'exchanger')
-PORT = os.environ.get('EXCHANGER_PORT', '5432')
-
 DATABASE_URI = f'postgres+psycopg2://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB}'
 
 engine = create_engine(DATABASE_URI)
