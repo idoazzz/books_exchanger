4a5299783e25f8ff320d839c87a3fa1bed3520a0
Ido <idoazzz@gmail.com>
2019-11-15 13:22:29 -0500

    Adding categories

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
+import logging
 from fastapi import FastAPI
 
+from app.db.config import recreate_database, transaction
+from app.db.models import Category
+
 app = FastAPI()
+logger = logging.getLogger("app")
+logger.setLevel(logging.DEBUG)
+
+logger.debug("Creating the database...")
+recreate_database()
+
+logger.debug("Init categories table...")
+with open("categories.txt") as file, transaction() as session:
+    categories = file.readlines()
+    for category in categories:
+        logger.debug("Adding category: %s", category)
+        session.add(Category(name=category))
+        session.commit()
+
+    session.query(Category).all()
 
 
 @app.get("/")
