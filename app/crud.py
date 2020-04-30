7f3ab779d104e464af5c53989c922ae89610a3e3
Ido <idoazzz@gmail.com>
2020-04-30 12:51:35 -0400

    Started nearest users method

--- app/crud.py
+++ app/crud.py
@@ -5,6 +5,8 @@ from app.db.config import Session
 from app.db.models import Category, User
 
 
+# Categories CRUD
+
 def get_all_categories(session: Session, limit: int = 100):
     """Get all categories from DB."""
     return session.query(Category).limit(limit).all()
@@ -14,6 +16,9 @@ def get_categories_by_name(session: Session, filter: str, limit: int = 100):
     """Get all categories that match to specific filter from DB."""
     return session.query(Category).filter(
         Category.name.contains(filter)).limit(limit).all()
+
+
+# Users CRUD
 
 
 def get_all_users(session: Session, limit: int = 100):
@@ -26,6 +31,17 @@ def get_users_by_name(session: Session, filter: str, limit: int = 100):
     return session.query(User).filter(
         User.name.contains(filter)).limit(limit).all()
 
+
+def get_user_by_email(session: Session, email):
+    """Get specifc user by email address."""
+    return session.query(User).filter_by(email=email).first()
+
+
+def get_near_users(session: Session, latitude, longitude, radius):
+    """Get specifc user by email address."""
+    pass
+
+
 def add_user(session: Session, password: str, name: str, email: str,
              address: str, latitude: int, longitude: int):
     """Add new user to the DB."""
@@ -36,14 +52,9 @@ def add_user(session: Session, password: str, name: str, email: str,
                     password=hashed_password,
                     latitude=latitude,
                     longitude=longitude)
+
     session.add(new_user)
     session.commit()
     # Retrieving new data like generated id.
     session.refresh(new_user)
     return new_user
-
-
-def get_user_by_email(session: Session, email):
-    """Get specifc user by email address."""
-    return session.query(User).filter_by(email=email).first()
-
--- app/db/models.py
+++ app/db/models.py
@@ -2,7 +2,7 @@
 import datetime
 
 from .base import Base
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from sqlalchemy import (Column, Integer, String, Date, ForeignKey,
                         DateTime, Float, LargeBinary)
 
