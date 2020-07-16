171886683e980d1f0c7779d57319b94cc7a09d4a
Ido Azulay <idoazzz@gmail.com>
2020-07-16 14:17:55 +0300

    Added monolith app into .ignore (seperating into microservices).

--- app/startup.sh
+++
@@ -1,5 +0,0 @@
-# alembic -c ./db/alembic.ini stamp head
-# alembic -c ./db/alembic.ini revision --autogenerate -m "Changes password length."
-# alembic -c ./db/alembic.ini upgrad1e head
-# pytest
-uvicorn main:app --host 0.0.0.0 --port 8080 --reload
