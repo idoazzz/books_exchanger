7859d4cf3c6d757f5f2db23c73bcbbbab59867db
Ido Azulay <idoazzz@gmail.com>
2020-07-20 15:45:09 +0300

    Fix alembic migrations in both services

--- services/users/app/db/tables.py
+++ services/users/app/db/tables.py
@@ -11,10 +11,10 @@ class User(Base):
     __tablename__ = 'users'
     categories_ids = Column(ARRAY(Integer))
     latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
-    name = Column(String(50), nullable=False)
+    longitude = Column(Integer, nullable=False)
+    name = Column(String(100), nullable=False)
     address = Column(String(100), nullable=False)
-    password = Column(String(50), nullable=False)
+    password = Column(String(100), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     id = Column(Integer, primary_key=True, autoincrement=True)
     join_date = Column(DateTime, default=datetime.datetime.utcnow)
